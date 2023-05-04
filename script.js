6bed8103268ba48454bf42fa989ffb7c92206b1e
Alex0247 <59071533+Alex0247@users.noreply.github.com>
2023-05-04 22:23:43 +0100

    Add CSS To Index.HTML With JS

---
+++ script.js
@@ -0,0 +1,11 @@
+function lasagna(){
+    location.href = "../recipes/lasagna.html";
+}
+
+function burger(){
+    location.href = "../recipes/burger.html";
+}
+
+function steak(){
+    location.href = "../recipes/steak.html";
+}
